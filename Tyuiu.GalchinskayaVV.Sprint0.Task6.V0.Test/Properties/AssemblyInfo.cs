a01314f30a8d6f057245cf503eccde070f9fb7b5
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-09-15 02:00:50 +0500

    Task6

---
+++ Tyuiu.GalchinskayaVV.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GalchinskayaVV.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GalchinskayaVV.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17a6ab8e-1f93-40c0-8330-cfe78f9e6621")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GalchinskayaVV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GalchinskayaVV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива =" + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность эллементов массива =" + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение эллементов массива =" +  DataService.MultiplicationArray(arraynums));
+
+            Console.ReadKey();
+        }
+    }
+}
