749e50cdb28c45dd6684267f9bc2d1525b04a6c8
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-09-14 21:37:27 +0500

    Task5

---
+++ Tyuiu.GalchinskayaVV.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace Tyuiu.GalchinskayaVV.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b ==0)
+            {
+                Console.WriteLine("на 0 делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Tyuiu.GalchinskayaVV.Sprint0.Task5.Vo.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GalchinskayaVV.Sprint0.Task5.Vo.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GalchinskayaVV.Sprint0.Task5.Vo.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b0f6b15-cbcb-4560-8e14-696326563182")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
